c05ab96149505225750d541f8178c71f35548f0a
Yovel Becker <yovelb13@gmail.com>
2020-01-09 18:27:49 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Loading from './components/Loading'
+const About = lazy(() => import('./views/About'))
+const Home = lazy(() => import('./views/Home'))
+const Header = lazy(() => import('./components/Header'))
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={Loading} >
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Loading: React.FC = () => {
+    return (
+        <h2>
+            Loading...
+        </h2>
+    )
+}
+
+export default Loading
